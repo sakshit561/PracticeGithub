53aab4c490ad0aa3580c1a225269808bdb30382f
Sakshi <sakshit561@gmail.com>
2022-12-13 23:34:45 +0530

    First commit

---
+++ src/test/java/RegistrationPage.java
@@ -0,0 +1,25 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RegistrationPage {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.shutterstock.com/");
+		Actions actions = new Actions(driver);
+		WebElement elementLocator = driver.findElement(By.xpath("//a[@data-automation=\"signupButton\"]"));
+		actions.contextClick(elementLocator).perform();
+		
+	}
+
+}
